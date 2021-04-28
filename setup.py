0d119295a0d7437fad4e98dd3f2f5e8afa61b0db
vn.py <xiaoyou.chen@foxmail.com>
2021-04-28 08:29:08 +0800

    [Mod] 更新接口的整体源代码

--- setup.py
+++ setup.py
@@ -1,18 +1,6 @@
-""""""
-
-import ast
 import platform
-import re
 
-from setuptools import Extension, find_packages, setup
-
-
-def get_version_string() -> str:
-    """获取版本号"""
-    with open("vnpy_ctp/__init__.py", "rb") as f:
-        content = f.read().decode("utf-8")
-        version_line = re.search(r"__version__\s+=\s+(.*)", content).group(1)
-        return str(ast.literal_eval(version_line))
+from setuptools import Extension, setup
 
 
 def get_ext_modules() -> list:
@@ -23,12 +11,12 @@ def get_ext_modules() -> list:
     Windows直接使用预编译的pyd即可
     Mac由于缺乏二进制库支持无法使用
     """
-    if platform.system != "Linux":
+    if platform.system() != "Linux":
         return []
 
     compiler_flags = [
-        "-std=c++17",  # standard
-        "-O3",  # Optimization
+        "-std=c++17",
+        "-O3",
         "-Wno-delete-incomplete", "-Wno-sign-compare",
     ]
     extra_link_args = ["-lstdc++"]
@@ -74,34 +62,5 @@ def get_ext_modules() -> list:
 
 
 setup(
-    name="vnpy",
-    version=get_version_string(),
-    author="vn.py team",
-    author_email="vn.py@foxmail.com",
-    license="MIT",
-    url="https://www.vnpy.com",
-    description="A framework for developing quant trading systems.",
-    long_description=__doc__,
-    keywords='quant quantitative investment trading algotrading',
-    include_package_data=True,
-    packages=find_packages(),
-    package_data={"": [
-        "*.dll",
-        "*.so",
-        "*.pyd",
-    ]},
-    install_requires=["vnpy"],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Operating System :: Microsoft :: Windows :: Windows 10",
-        "Operating System :: Microsoft :: Windows :: Windows Server 2019",
-        "Operating System :: POSIX :: Linux"
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Topic :: Office/Business :: Financial :: Investment",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: Chinese (Simplified)"
-    ],
     ext_modules=get_ext_modules(),
 )
