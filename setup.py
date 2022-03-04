131f2e2abfbb12bc32633ae4752b73b800ce63a6
Edanflame <237397488@qq.com>
2022-03-04 14:14:25 +0800

    [Mod] 调整安装脚本setup.py，支持Windows下安装时根据Python版本进行编译

--- setup.py
+++ setup.py
@@ -7,20 +7,22 @@ def get_ext_modules() -> list:
     """
     获取三方模块
 
-    Linux需要编译封装接口
-    Windows直接使用预编译的pyd即可
+    Linux和Windows需要编译封装接口
     Mac由于缺乏二进制库支持无法使用
     """
-    if platform.system() != "Linux":
-        return []
-
-    compiler_flags = [
-        "-std=c++17",
-        "-O3",
-        "-Wno-delete-incomplete", "-Wno-sign-compare",
-    ]
-    extra_link_args = ["-lstdc++"]
-    runtime_library_dirs = ["$ORIGIN"]
+    if platform.system() == "Linux":
+        extra_compile_flags = [
+            "-std=c++17",
+            "-O3",
+            "-Wno-delete-incomplete",
+            "-Wno-sign-compare",
+        ]
+        extra_link_args = ["-lstdc++"]
+        runtime_library_dirs = ["$ORIGIN"]
+    else:
+        extra_compile_flags = ["-O2", "-MT"]
+        extra_link_args = []
+        runtime_library_dirs = []
 
     vnctpmd = Extension(
         "vnpy_ctptest.api.vnctpmd",
@@ -33,7 +35,7 @@ def get_ext_modules() -> list:
         undef_macros=[],
         library_dirs=["vnpy_ctptest/api/libs", "vnpy_ctptest/api"],
         libraries=["thostmduserapi_se", "thosttraderapi_se"],
-        extra_compile_args=compiler_flags,
+        extra_compile_args=extra_compile_flags,
         extra_link_args=extra_link_args,
         runtime_library_dirs=runtime_library_dirs,
         depends=[],
@@ -51,7 +53,7 @@ def get_ext_modules() -> list:
         undef_macros=[],
         library_dirs=["vnpy_ctptest/api/libs", "vnpy_ctptest/api"],
         libraries=["thostmduserapi_se", "thosttraderapi_se"],
-        extra_compile_args=compiler_flags,
+        extra_compile_args=extra_compile_flags,
         extra_link_args=extra_link_args,
         runtime_library_dirs=runtime_library_dirs,
         depends=[],
